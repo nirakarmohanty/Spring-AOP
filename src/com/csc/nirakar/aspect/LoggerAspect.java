c1255c9f50e38edf75465cc922307b5cd8d0d044
Nirakar Mohanty <nirakarmohanty@gmail.com>
2015-09-30 16:09:23 +0530

    added new Advise

--- src/com/csc/nirakar/aspect/LoggerAspect.java
+++ src/com/csc/nirakar/aspect/LoggerAspect.java
@@ -4,10 +4,17 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
 
+/**This is a Demo class which is called as an ASPECT
+ * Methods are called Advise
+ * @author nmohanty
+ *
+ */
 @Aspect
 public class LoggerAspect {
 	
-	// Executes all "public String getName()" method present in Bean
+	/** Executes all "public String getName()" method present in Bean.
+	 * This method is can be treated as an advice which will be getting called before getName method
+	*/
 	@Before("execution(public String getName())")
 	public void displayLoggerForgetName(){
 		System.out.println("Logger is called from here");
@@ -19,10 +26,15 @@ public class LoggerAspect {
 		System.out.println("Logger is called from here");
 	}
 	
-	//Executes package Specific method
+	//Executes package Specific method i.e for a specific Class
 	@Before("execution(public String com.csc.nirakar.bean.Employee.getName())")
 	public void displayLoggerForSpecificPackage(){
 		System.out.println("Logger is called from here");
+	}
+	
+	
+	public void loggingAdvice(){
+		
 	}
 	
 	
